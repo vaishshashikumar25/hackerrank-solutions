b19408113cfb0bfe08f5cf4a511ceef47f572527
vaishshashikumar25 <vaish.shashikumar25@gmail.com>
2026-09-18 09:36:24 +0530

    Add hackerrank/reduced-string solution at 2026-09-18_09-36-21

---
+++ hackerrank/reduced-string/2026-09-18_09-36-21/solution.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superReducedString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def superReducedString(s):
+    stack = []
+
+    for ch in s:
+        if stack and stack[-1] == ch:
+            stack.pop()
+        else:
+            stack.append(ch)
+
+    return ''.join(stack) if stack else "Empty String"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
